239c92b2ffa19367a0ea743cce6ca223c27feb5a
Nammiiro Zaharah bidin <zaharabidin485@gmail.com>
2024-10-24 18:13:05 +0300

    hard

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,23 @@
-// eslint.config.js
 import { defineConfig } from 'eslint-define-config';
 
-export default defineConfig({
-    overrides: [
-        {
-            files: ['*.js', '*.vue'],
-            rules: {
-                 'no-debugger': 'error',
-                 'curly': 'error',
-                 'vue/no-unused-components': 'warn',
-                 'vue/require-default-prop': 'error',		
-            },
+export default defineConfig([
+    {
+        ignores: ['public/vendor/jquery/*'], // Ignore jQuery files
+        rules: {
+            'no-debugger': 'error', // Disallow debugger statements
+            // Add any other global rules here
         },
-    ],
-});
+    },
+    {
+        files: ['*.js', '*.vue'],
+        rules: {
+            // Add any other project-specific rules here
+        },
+    },
+    {
+        files: ['public/vendor/jquery/*.js'],
+        rules: {
+            'no-unused-expressions': 'off', // Disable for jQuery files specifically
+        },
+    },
+]);
